febc6330ca59a9bf1eee53435352cf0488a0d56a
jahammo2 <jahammo2@gmail.com>
2015-06-01 10:29:50 -0400

    9:21

--- coinchanger/day18.rb
+++ coinchanger/day18.rb
@@ -1,3 +1,33 @@
+class CoinChanger
+  attr_accessor :change
+
+  # def initialize
+  #   @change = []
+  # end
+
+  def create_change(num)
+    @change = []
+    remainder = num % 25
+    num -= remainder
+    coins(num,25)
+    coins(remainder,10)
+    remainder %= 10
+    coins(remainder,5)
+    remainder %= 5
+    coins(remainder,1)
+    @change
+  end
+
+  def coins(val,coin)
+    length = val/coin
+    while length >= 1
+      change.push(coin)
+      length -= 1
+    end
+  end
+
+end
+
 require 'spec_helper'
 require_relative '../today'
 
@@ -25,3 +55,5 @@ describe CoinChanger do
   end
 
 end
+
+
--- today.rb
+++ today.rb
@@ -1,27 +1,38 @@
 class CoinChanger
   attr_accessor :change
 
+  # def initialize
+  #   @change = []
+  # end
+
   def create_change(num)
     @change = []
     remainder = num % 25
     num -= remainder
     coins(num,25)
-    coins(remainder, 10)
-    puts remainder
+    coins(remainder,10)
     remainder %= 10
-    puts remainder
-    coins(remainder, 5)
+    coins(remainder,5)
     remainder %= 5
-    coins(remainder, 1)
+    coins(remainder,1)
     @change
   end
 
   def coins(val,coin)
     length = val/coin
     while length >= 1
-      @change.push(coin)
+      change.push(coin)
       length -= 1
     end
   end
+
+
+
+
+
+
+
+
+
 
 end
