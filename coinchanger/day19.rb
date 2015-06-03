5ed52f2bcc1fcd5404cfb2cd99c0264cf226a72e
jahammo2 <jahammo2@gmail.com>
2015-06-03 14:18:06 -0400

    I have not refactored this at all. It's ugly, atrocious and awful. But it's passing. Tomorrow for this exercise, I'll refactor.

--- coinchanger/day19.rb
+++ coinchanger/day19.rb
@@ -1,3 +1,99 @@
+class RomanNumeralChanger
+  attr_accessor :letters
+  attr_reader :number
+  
+  def initialize
+    @letters
+    @number
+  end
+
+  def create_letter(num)
+    letters = []
+    last_two_digits = num % 100
+    last_digit = last_two_digits % 10
+    second_digit = last_two_digits - last_digit
+    # the number that is still divisible by 100 so that I can put in C's
+    remaining_digits = num - last_two_digits
+
+    letters.push(letterize_remaining_digits(remaining_digits))
+    letters.push(letterize_second_digit(second_digit))
+    letters.push(letterize_last_digit(last_digit))
+
+
+    
+    letters.join('')
+  end
+
+  def letterize_remaining_digits(num)
+    remainding_digits_letter_holder = []
+    (num/100).times { remainding_digits_letter_holder.push('C') }
+    remainding_digits_letter_holder
+  end
+
+  def letterize_second_digit(num)
+    case num
+    when 10
+      'X'
+    when 20
+      'XX'
+    when 30
+      'XXX'
+    when 40
+      'XL'
+    when 50
+      'L'
+    when 60
+      'LX'
+    when 70
+      'LXX'
+    when 80
+      'LXXX'
+    when 90
+      'XC'
+    else
+      ''
+    end
+  end
+
+  def letterize_last_digit(num)
+    case num
+    when 1
+      'I'
+    when 2
+      'II'
+    when 3
+      'III'
+    when 4
+      'IV'
+    when 5
+      'V'
+    when 6
+      'VI'
+    when 7
+      'VII'
+    when 8
+      'VIII'
+    when 9
+      'IX'
+    else
+      ''
+    end
+  end
+
+  def set_num_below_100(num)
+    num % 100
+  end
+
+  def pass_in_letter(single_num)
+    letters = "#{single_num}"
+    letters.to_s
+  end
+
+
+
+
+end
+
 require 'spec_helper'
 require_relative '../today'
 
@@ -68,3 +164,5 @@ describe RomanNumeralChanger do
     expect(roman_numeral_changer.create_letter(0)).to eq('')
   end
 end
+
+
--- today.rb
+++ today.rb
@@ -1,36 +1,93 @@
-class CoinChanger
-  attr_accessor :change
-
-  # def initialize
-  #   @change = []
-  # end
-
-  def create_change(num)
-    @change = []
-    remainder = num % 25
-    num -= remainder
-    coins(num,25)
-    coins(remainder,10)
-    remainder %= 10
-    coins(remainder,5)
-    remainder %= 5
-    coins(remainder,1)
-    @change
+class RomanNumeralChanger
+  attr_accessor :letters
+  attr_reader :number
+  
+  def initialize
+    @letters
+    @number
   end
 
-  def coins(val,coin)
-    length = val/coin
-    while length >= 1
-      change.push(coin)
-      length -= 1
+  def create_letter(num)
+    letters = []
+    last_two_digits = num % 100
+    last_digit = last_two_digits % 10
+    second_digit = last_two_digits - last_digit
+    # the number that is still divisible by 100 so that I can put in C's
+    remaining_digits = num - last_two_digits
+
+    letters.push(letterize_remaining_digits(remaining_digits))
+    letters.push(letterize_second_digit(second_digit))
+    letters.push(letterize_last_digit(last_digit))
+
+
+    
+    letters.join('')
+  end
+
+  def letterize_remaining_digits(num)
+    remainding_digits_letter_holder = []
+    (num/100).times { remainding_digits_letter_holder.push('C') }
+    remainding_digits_letter_holder
+  end
+
+  def letterize_second_digit(num)
+    case num
+    when 10
+      'X'
+    when 20
+      'XX'
+    when 30
+      'XXX'
+    when 40
+      'XL'
+    when 50
+      'L'
+    when 60
+      'LX'
+    when 70
+      'LXX'
+    when 80
+      'LXXX'
+    when 90
+      'XC'
+    else
+      ''
     end
   end
 
+  def letterize_last_digit(num)
+    case num
+    when 1
+      'I'
+    when 2
+      'II'
+    when 3
+      'III'
+    when 4
+      'IV'
+    when 5
+      'V'
+    when 6
+      'VI'
+    when 7
+      'VII'
+    when 8
+      'VIII'
+    when 9
+      'IX'
+    else
+      ''
+    end
+  end
 
+  def set_num_below_100(num)
+    num % 100
+  end
 
-
-
-
+  def pass_in_letter(single_num)
+    letters = "#{single_num}"
+    letters.to_s
+  end
 
 
 
